a20b8b68d89a6cba212c4e95b99aa008ed47f92b
sinha08 <aks.sinha6@gmail.com>
2018-11-24 22:49:15 +0530

    Initial checkin of planit code

---
+++ app/src/main/java/planit/sinha/ankur/com/planit/usecase/CreateTask.java
@@ -0,0 +1,8 @@
+package planit.sinha.ankur.com.planit.usecase;
+
+/**
+ * Created by ankur sinha on 24-11-2018.
+ */
+
+public class CreateTask {
+}
---
+++ app/src/main/java/planit/sinha/ankur/com/planit/utils/AppUtils.java
@@ -0,0 +1,8 @@
+package planit.sinha.ankur.com.planit.utils;
+
+/**
+ * Created by ankur sinha on 24-11-2018.
+ */
+
+public class AppUtils {
+}
